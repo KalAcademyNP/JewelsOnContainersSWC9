988e380c8771013056dad569b1ddbe24226e53e0
Kal <info@kalacademy.org>
2021-03-21 11:08:31 -0700

    Complete catalog microservice

--- ProductCatalogAPI/Controllers/CatalogController.cs
+++ ProductCatalogAPI/Controllers/CatalogController.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using ProductCatalogAPI.Data;
+using ProductCatalogAPI.Domain;
 
 namespace ProductCatalogAPI.Controllers
 {
@@ -14,9 +16,11 @@ namespace ProductCatalogAPI.Controllers
     public class CatalogController : ControllerBase
     {
         private readonly CatalogContext _context;
-        public CatalogController(CatalogContext context)
+        private readonly IConfiguration _config;
+        public CatalogController(CatalogContext context, IConfiguration config)
         {
             _context = context;
+            _config = config;
         }
 
         [HttpGet("[action]")]
@@ -30,7 +34,63 @@ namespace ProductCatalogAPI.Controllers
                     .Take(pageSize)
                     .ToListAsync();
 
+            items = ChangePictureUrl(items);
+
+            return Ok(items);
 
         }
+
+        [HttpGet("[action]/type/{catalogTypeId}/brand/{catalogBrandId}")]
+        public async Task<IActionResult> Items(
+            int? catalogTypeId,
+            int? catalogBrandId,
+           [FromQuery]int pageIndex = 0,
+           [FromQuery]int pageSize = 6)
+        {
+            var query = (IQueryable<CatalogItem>)_context.Catalog;
+            if (catalogTypeId.HasValue)
+            {
+                query = query.Where(c => c.CatalogTypeId == catalogTypeId);
+            }
+            if (catalogBrandId.HasValue)
+            {
+                query = query.Where(c => c.CatalogBrandId == catalogBrandId);
+            }
+
+            var items = await query
+                    .OrderBy(c => c.Name)
+                    .Skip(pageIndex * pageSize)
+                    .Take(pageSize)
+                    .ToListAsync();
+
+            items = ChangePictureUrl(items);
+
+            return Ok(items);
+
+        }
+
+        private List<CatalogItem> ChangePictureUrl(List<CatalogItem> items)
+        {
+            items.ForEach(item =>
+                item.PictureUrl = item.PictureUrl.Replace("http://externalcatalogbaseurltobereplaced",
+                    _config["ExternalCatalogBaseUrl"]));
+            return items;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> CatalogTypes()
+        {
+            var types = await _context.CatalogTypes.ToListAsync();
+            return Ok(types);
+        }
+
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> CatalogBrands()
+        {
+            var brands = await _context.CatalogBrands.ToListAsync();
+            return Ok(brands);
+        }
+
     }
 }
